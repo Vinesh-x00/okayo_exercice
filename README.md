d51b49492b874558578ad4c6b589a44343bf9f1b
vineshR <vinesh.rengassamy@edu.univ-eiffel.fr>
2025-12-07 11:14:17 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# okayo_exercice
---
+++ src/test/java/fr/okayo/exercice/ExerciceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.okayo.exercice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
