aa9cd6abe8793e9cedbd8f01c0919129717f2202
vineshR <vinesh.rengassamy@edu.univ-eiffel.fr>
2025-12-07 22:49:59 +0100

    setup.sh deleted

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-su postgres -c "pg_ctl -D /var/lib/postgresql/data -o \"-c listen_addresses='*'\" -w start"
-sleep 3
-mvn -f /app/pom.xml -DskipTests package
-exec java -jar /app/target/*.jar
