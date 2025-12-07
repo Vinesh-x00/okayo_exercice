c93b2a47447fb85009d85b5abc6f3b979349316e
vineshR <vinesh.rengassamy@edu.univ-eiffel.fr>
2025-12-07 12:43:04 +0100

    coustomer entity added

---
+++ src/main/java/fr/okayo/exercice/coustomer/Customer.java
@@ -0,0 +1,41 @@
+package fr.okayo.exercice.coustomer;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Table(name = "Client")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Customer {
+
+    @Id
+    @Column(name = "id_client")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    private Integer id;
+
+    @Column(name = "code_client", length = 100, unique = true)
+    @NotNull
+    private String customerCode;
+
+    @Column(name = "nom", nullable = false, length = 100)
+    @NotNull
+    private String name;
+
+    @Column(name = "adresse", length = 500)
+    @NotNull
+    private String address;
+
+    @Column(name = "code_postal", length = 50)
+    @NotNull
+    private String zipCode;
+
+    @Column(name = "ville", length = 100)
+    @NotNull
+    private String city;
+}
